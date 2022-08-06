4ce90c079ace5cb1cbc09c5ad5f9a587e789be6a
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2022-08-06 14:18:04 +0530

    Adding trie Api

--- kata/src/main/java/com/structures/kata/TrieNode.java
+++ kata/src/main/java/com/structures/kata/TrieNode.java
@@ -1,28 +1,73 @@
 package com.structures.kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrieNode {
-	public String val;
-	public TrieNode left;
-	public TrieNode right;
+	public TrieNode next;
+	public Character val;
+	public List<TrieNode> listOfNodesAttached = new ArrayList<TrieNode>();
 
-	public boolean wordComplete;
+	public boolean wordComplete = false;
 
 
-	TrieNode() {
+	public TrieNode() {
 	}
 
-	TrieNode(String val) {
+	public TrieNode(boolean wordComplete) {
+		super();
+		this.wordComplete = wordComplete;
+	}
+
+
+	public TrieNode(Character val, boolean wordComplete) {
+		super();
 		this.val = val;
+		this.wordComplete = wordComplete;
 	}
 
-	public TrieNode(String val, TrieNode left, TrieNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
+
+
+	public TrieNode(TrieNode next, List<TrieNode> listOfChars, boolean wordComplete) {
+		super();
+		this.next = next;
+		this.listOfNodesAttached = listOfChars;
+		this.wordComplete = wordComplete;
 	}
+
+
+
 
 	@Override
 	public String toString() {
-		return "TrieNode [val=" + val + ", left=" + left + ", right=" + right + "]";
+		if (!this.listOfNodesAttached.isEmpty()) {
+		// @formatter:off
+		return 		
+				"TrieNode : [val : " 
+				+ val 
+				+ ", wordComplete : "
+				+ wordComplete 
+				+ ", \n\t\t\t## next --> ["
+				+ next
+				+ ", \nwordsOriginating : " 
+				+ listOfNodesAttached.size()
+				+ ", \n\tlinkedWords : " 
+				+ listOfNodesAttached
+				+ "]";
+	// @formatter:on
+		} else {
+		// @formatter:off
+		return 		
+				"TrieNode : [val : " 
+				+ val 
+				+ ", wordComplete : "
+				+ wordComplete 
+				+ ", \n\t## next --> ["
+				+ next
+				+ "]";
+// @formatter:on
+		}
+
+
 	}
 }
