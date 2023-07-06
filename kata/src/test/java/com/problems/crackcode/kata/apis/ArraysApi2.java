4890929499cd6031553d859430eef9b87acd48e0
Yuvraj Sharma <yuvraj.sharma@statusneo.com>
2023-07-06 18:25:44 +0530

    update

--- kata/src/test/java/com/problems/crackcode/kata/apis/ArraysApi2.java
+++ kata/src/test/java/com/problems/crackcode/kata/apis/ArraysApi2.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -2601,6 +2602,111 @@ public class ArraysApi2 {
 			start++;
 			end--;
 		}
+	}
+
+	// @formatter:off
+	/**
+	 * Given n non-negative integers representing an elevation map where
+	 * the width of each bar is 1, compute how much water it can trap
+	 * after raining.
+	 *
+	 *
+	 *
+	 * Example 1:
+	 * link : https://leetcode.com/problems/trapping-rain-water/
+	 *
+	 * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+	 * Output: 6
+	 * Explanation: The above elevation map (black section)
+	 * is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+	 * In this case, 6 units of rain water (blue section) are being trapped.
+	 *
+	 *
+	 * Example 2:
+	 *
+	 * Input: height = [4,2,0,3,2,5]
+	 * Output: 9
+	 */
+	// @formatter:on
+	@Test
+	@DisplayName("Test Water Can Be Trapped")
+	void testWaterCanBeTrapped() {
+		int[] a = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+		int op = findWaterThatCanBeTrapped(a);
+		Assertions.assertEquals(6, op);
+	}
+
+
+	@Test
+	@DisplayName("Test Water Can Be Trapped")
+	void testWaterCanBeTrappe4() {
+		int[] a = { 2, 1, 2, 3 };
+		int op = findWaterThatCanBeTrapped(a);
+		Assertions.assertEquals(1, op);
+	}
+
+
+	@Test
+	@DisplayName("Test Water Can Be Trapped")
+	void testWaterCanBeTrappe5() {
+		int[] a = { 2, 1, 1, 3 };
+		int op = findWaterThatCanBeTrapped(a);
+		Assertions.assertEquals(2, op);
+	}
+
+	@Test
+	@DisplayName("Test Water Can Be Trapped")
+	void testWaterCanBeTrapped1() {
+		int[] a = { 4, 2, 0, 3, 2, 5 };
+		int op = findWaterThatCanBeTrapped(a);
+		Assertions.assertEquals(9, op);
+	}
+
+	@Test
+	@DisplayName("Test Water Can Be Trapped")
+	void testWaterCanBeTrapped2() {
+		int[] a = { 2, 1, 0, 1, 2 };
+		int op = findWaterThatCanBeTrapped(a);
+		Assertions.assertEquals(4, op);
+	}
+
+
+	@Test
+	@DisplayName("Test Water Can Be Trapped")
+	void testWaterCanBeTrapped6() {
+		int[] a = { 4, 2, 3 };
+		int op = findWaterThatCanBeTrapped(a);
+		Assertions.assertEquals(1, op);
+	}
+
+
+	int findWaterThatCanBeTrapped(int[] a) {
+		//HC : Holding Capacity
+		int tHC = 0;
+		Stack<Integer> st = new Stack<>();
+		for (int i = a.length - 1; i >= 0; i--) {
+			st.push(i);
+		}
+
+		while (!st.isEmpty()) {
+			int currHC = 0;
+			int pop = st.pop();
+			int i = pop + 1;
+			while (i < a.length && !st.empty() && a[pop] > a[i]) {
+				currHC += a[pop] - a[i];
+				i++;
+			}
+			if (i < a.length && a[i] >= a[pop]) {
+				int j = pop + 1;
+				while (j < i) {
+					st.pop();
+					j++;
+				}
+				tHC += currHC;
+			}
+		}
+
+		return tHC;
 	}
 }
 
